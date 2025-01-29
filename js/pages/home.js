557c0ecfb140c7daed6c6aeaf4a182bb45d7ccc7
Guillermo-Mora <guimormor@alu.edu.gva.es>
2025-01-29 20:49:32 +0100

    Mejora en el funcionamiento del pop-up del CV y el pop-up del video. He reemplazado el uso de visibility hidden para evitar tener que cargar constantemente estos contenidos cuando no se estan usando. En su lugar, se mantienen por defecto con display none. Al registrar click sobre el elemento con la id que lo abre, se añaden las clases que desactivan el scroll de la pagina y que pasan el elemento a display flex. Tras esto, con un setTimeout, se espera 10 ms hasta darle la clase que le da la propiedad de opacity 1. Esto combinado con transition, permite seguir cargando la transicion fluida en entrada y salida sin tener que estar cargando los elementos cuando no se estan usando.

--- js/pages/home.js
+++ js/pages/home.js
@@ -7,11 +7,44 @@ const cuerpo = document.querySelector('.scroll-activo');
 abrirCV.addEventListener('click', () => {
     divCV.classList.toggle('contenedor-cv-activado');
     cuerpo.classList.toggle('scroll-inactivo');
+    setTimeout(() => {
+        divCV.classList.toggle('contenedor-cv-visible');
+    }, 10)
 });
 
 const cerrarMenuCV = document.querySelector('.cerrar-cv');
 
 cerrarMenuCV.addEventListener('click', () => {
-    divCV.classList.remove('contenedor-cv-activado');
-    cuerpo.classList.remove('scroll-inactivo');
+    divCV.classList.remove('contenedor-cv-visible');
+    setTimeout(() => {
+        divCV.classList.remove('contenedor-cv-activado');
+        cuerpo.classList.remove('scroll-inactivo');
+    }, 300)
 });
+
+/*---*/
+
+/*Abrir y cerrar video*/
+
+const abrirVideo = document.querySelector('#abrirVideo');
+const divVideo = document.querySelector('.contenedor-video');
+
+abrirVideo.addEventListener('click', () => {
+    divVideo.classList.toggle('contenedor-video-activado');
+    cuerpo.classList.toggle('scroll-inactivo');
+    setTimeout(() => {
+        divVideo.classList.toggle('contenedor-video-visible');
+    }, 10)
+});
+
+const cerrarMenuVideo = document.querySelector('.cerrar-video');
+
+cerrarMenuVideo.addEventListener('click', () => {
+    divVideo.classList.remove('contenedor-video-visible');
+    setTimeout(() => {
+        divVideo.classList.remove('contenedor-video-activado');
+        cuerpo.classList.remove('scroll-inactivo');
+    }, 300)
+});
+
+/*---*/
